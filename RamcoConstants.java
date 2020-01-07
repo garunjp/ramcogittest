338b74e9ea4e5447614b0d7107b0fa86a57a618b
arunjp <arunjayaprakash.g@ramco.com>
2020-01-07 14:37:53 +0530

    My first Commit

---
+++ RamcoConstants.java
@@ -0,0 +1,9 @@
+package org.wso2.apimgt.keymgt.ramco.util;
+
+public class RamcoConstants {
+    public static final String CONSUMER_KEY_IDENTIFIER="RamcoJWTConsumerKeyIdentifier";
+    public static final String USER_NAME_IDENTIFIER = "RamcoJWTUserNameIdentifier";
+    public static final String JWK_URL = "RamcoJWKURL";
+    public static final String PUBLIC_KEY_ID  = "RamcoPublicKeyId";
+    public static final String RAMCO_TOKEN_API_URL = "RamcoJWTUserNameIdentifier";
+}
